60cc21559c64a63a6c24f1c8843898f934418658
Antony <a.krutenok@gmail.com>
2022-12-03 15:59:04 +0300

    feat: implement read.js using Readable Stream and process.stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileName = 'fileToRead.txt';
+    const filePath = `${__dirname}/files/${fileName}`;
+
+    const fileReadStream = (await fs.open(filePath)).createReadStream();
+
+    fileReadStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
