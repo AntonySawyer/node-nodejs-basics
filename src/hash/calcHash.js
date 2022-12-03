9c56f70ba7b588c6021486d878d05895ad165499
Antony <a.krutenok@gmail.com>
2022-12-03 15:59:04 +0300

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+
+    const fileToHashName = 'fileToCalculateHashFor.txt';
+    const fileToHashPath = `${__dirname}/files/${fileToHashName}`;
+
+    fs.readFile(fileToHashPath)
+        .then((fileData) => {
+            return crypto.createHash('sha256')
+                .update(fileData);
+        })
+        .then((result) => {
+            console.log('result: ', result.digest('hex'))
+        })
+        .catch(() => {
+            throw Error('FS operation failed: can not open file');
+        });
+
+
+
 };
 
 await calculateHash();
