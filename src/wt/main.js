968cab967d4d5a42043555f4402b984389cb3896
Antony <a.krutenok@gmail.com>
2022-12-03 15:59:04 +0300

    feat: implement worker threads logic

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import path from 'node:path';
+import os from 'os';
+import wt from 'worker_threads';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const workerPath = `${__dirname}/worker.js`;
+
+    const numOfCpus = os.cpus().length;
+    const STARTING_NUMBER = 10;
+
+    const getPromiseResolve = (workerData) => {
+        return (resolve) => {
+            const worker = new wt.Worker(workerPath, { workerData });
+            worker.on('message', (data) => (
+                resolve({ status: 'resolved', data })
+            ));
+            worker.on('error', () => (
+                resolve({ status: 'error', data: null })
+            ));
+        }
+    }
+
+    const promises = new Array(numOfCpus).fill().map((_val, index) => (
+        new Promise(getPromiseResolve(STARTING_NUMBER + index))
+    ));
+
+    const promiseSettledResult = await Promise.allSettled(promises);
+    const result = promiseSettledResult.map((value) => value.value);
+
+    console.log(result);
 };
 
 await performCalculations();
