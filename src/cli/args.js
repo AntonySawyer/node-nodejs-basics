9850fcf4287f4fdb7da56929a4b5fde34020a744
Antony <a.krutenok@gmail.com>
2022-12-03 15:59:04 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    const varPrefix = '--';
+    const prefixedArgs = process.argv.slice(' ');
+
+    const result = prefixedArgs.reduce((output, currentValue, currentIndex, array) => {
+        if (currentValue.includes(varPrefix)) {
+            const valueToPrint = `${currentValue} is ${array[currentIndex + 1]}`;
+
+            return [...output, valueToPrint];
+        }
+
+        return output;
+    }, []);
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
