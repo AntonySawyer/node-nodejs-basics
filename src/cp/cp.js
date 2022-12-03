5fbe814b18104ecb3f467dd6193e1524aa2e1806
Antony <a.krutenok@gmail.com>
2022-12-03 15:59:04 +0300

    feat: implement child process logic

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import path from 'node:path';
+import cp from 'child_process';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = `${__dirname}/files/script.js`;
+
+    cp.fork(filePath, args, { stdio: 'inherit' });
 };
 
 spawnChildProcess();
