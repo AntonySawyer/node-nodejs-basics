9f4495b7295ff2ec997f7f6d830b26e097da53c2
Antony <a.krutenok@gmail.com>
2022-12-03 15:59:04 +0300

    feat: implement write.js using Write Stream and stdin

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileName = 'fileToWrite.txt';
+    const filePath = `${__dirname}/files/${fileName}`;
+
+    const fileWriteStream = (await fs.open(filePath, 'w')).createWriteStream();
+
+    process.stdin.pipe(fileWriteStream);
 };
 
 await write();
