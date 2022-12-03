d4ddd79e3546df1fccc146bf8c704bfa7e961d26
Antony <a.krutenok@gmail.com>
2022-12-03 15:59:04 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const rssVariables = Object.keys(process.env).filter((variable) => variable.includes(prefix));
+
+    const result = rssVariables
+        .map((variable) => (
+            `${variable}=${process.env[variable]}`
+        ))
+        .join('; ')
+
+    console.log(result);
 };
 
 parseEnv();
